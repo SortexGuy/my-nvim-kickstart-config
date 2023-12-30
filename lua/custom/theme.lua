2a294ec5ddd24a22cee1282fb7a9b31cea86773a
Rubén A <sortexjuegos@gmail.com>
2023-12-30 15:54:15 -0400

    Cambios de temas

--- lua/custom/theme.lua
+++ lua/custom/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme monokai-pro")
-vim.g.theme_id = 2
+vim.cmd("colorscheme catppuccin")
+vim.g.theme_id = 1
 -- end themery block
