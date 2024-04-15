8355df5903bf8fc90dbafbb09960c0291b46d0f4
Rubén A <sortexjuegos@gmail.com>
2024-04-14 23:11:05 -0400

    fix(lint): Por ahora

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,7 +4,7 @@ return { -- Linting
   config = function()
     local lint = require 'lint'
     lint.linters_by_ft = {
-      markdown = { 'markdownlint' },
+      -- markdown = { 'markdownlint' },
     }
 
     -- To allow other plugins to add linters to require('lint').linters_by_ft,
