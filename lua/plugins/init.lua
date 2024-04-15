3c38960d4af4dc8e6a1d70c08de0697d3dbbcb56
Rubén A <sortexjuegos@gmail.com>
2024-04-15 18:02:22 -0400

    feat: Dial.nvim added

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -80,4 +80,34 @@ return {
       }
     end,
   },
+  {
+    'monaqa/dial.nvim',
+    config = function()
+      local dial_map = require 'dial.map'
+      vim.keymap.set('n', '<A-k>', function()
+        dial_map.manipulate('increment', 'normal')
+      end)
+      vim.keymap.set('n', '<A-j>', function()
+        dial_map.manipulate('decrement', 'normal')
+      end)
+      vim.keymap.set('n', 'g<C-a>', function()
+        dial_map.manipulate('increment', 'gnormal')
+      end)
+      vim.keymap.set('n', 'g<C-x>', function()
+        dial_map.manipulate('decrement', 'gnormal')
+      end)
+      vim.keymap.set('v', '<C-a>', function()
+        dial_map.manipulate('increment', 'visual')
+      end)
+      vim.keymap.set('v', '<C-x>', function()
+        dial_map.manipulate('decrement', 'visual')
+      end)
+      vim.keymap.set('v', 'g<C-a>', function()
+        dial_map.manipulate('increment', 'gvisual')
+      end)
+      vim.keymap.set('v', 'g<C-x>', function()
+        dial_map.manipulate('decrement', 'gvisual')
+      end)
+    end,
+  },
 }
