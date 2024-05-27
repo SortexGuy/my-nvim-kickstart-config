d4b50a8a600541857c63de794858b1fd8a98b991
Rubén A <sortexjuegos@gmail.com>
2024-05-27 00:26:53 -0400

    feat: more hyprland

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -203,6 +203,19 @@ return {
           end,
         },
       }
+
+      -- Hyprlang LSP
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+        pattern = { '.*/hypr/.*%.conf', '*.hl', 'hypr*.conf' },
+        callback = function(event)
+          print(string.format('starting hyprls for %s', vim.inspect(event)))
+          vim.lsp.start {
+            name = 'hyprlang',
+            cmd = { 'hyprls' },
+            root_dir = vim.fn.getcwd(),
+          }
+        end,
+      })
     end,
   },
 }
