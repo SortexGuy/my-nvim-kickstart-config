3d066a60ca85bb072d98db8eb01bc9f680bcedca
Rubén A <sortexjuegos@gmail.com>
2024-05-30 19:31:58 -0400

    feat: Inlay hints not working

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -127,4 +127,13 @@ return {
       end)
     end,
   },
+  {
+    'MysticalDevil/inlay-hints.nvim',
+    event = 'LspAttach',
+    dependencies = { 'neovim/nvim-lspconfig' },
+    opts = {
+      commands = { enable = true }, -- Enable InlayHints commands, include `InlayHintsToggle`, `InlayHintsEnable` and `InlayHintsDisable`
+      autocmd = { enable = true }, -- Enable the inlay hints on `LspAttach` event
+    },
+  },
 }
