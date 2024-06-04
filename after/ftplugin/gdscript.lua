9adcb232bd4f4799c89ded0085da7059828f46ff
Rubén A <sortexjuegos@gmail.com>
2024-06-03 22:05:20 -0400

    feat(godot): Windows pipe

--- after/ftplugin/gdscript.lua
+++ after/ftplugin/gdscript.lua
@@ -1,6 +1,11 @@
 local port = os.getenv 'GDScript_Port' or '6005'
 local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
-local pipe = '/home/sortextg/tmp/godot.pipe' -- I use /tmp/godot.pipe
+local pipe
+if jit.os == 'Windows' then
+  pipe = [[C:\Temp\godot.pipe]]
+else
+  pipe = '/home/sortextg/tmp/godot.pipe' -- I use /tmp/godot.pipe
+end
 
 vim.lsp.start {
   name = 'Godot',
