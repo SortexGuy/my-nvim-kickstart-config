ef5ec6b2ad6051ba8a674631a7e73e261b5b11e5
Rubén A <sortexjuegos@gmail.com>
2024-06-08 13:58:02 -0400

    fix(gdscript): lsp for Godot

--- after/ftplugin/gdscript.lua
+++ after/ftplugin/gdscript.lua
@@ -1,14 +1,15 @@
 local port = os.getenv 'GDScript_Port' or '6005'
-local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
+local cmd = {[[C:\Program Files (x86)\Nmap\ncat.exe]], '127.0.0.1', port}
+-- vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
 local pipe
 if jit.os == 'Windows' then
-  pipe = [[C:\Temp\godot.pipe]]
+  pipe = [[C:\Temp\\godot.pipe]]
 else
   pipe = '/home/sortextg/tmp/godot.pipe' -- I use /tmp/godot.pipe
 end
 
 vim.lsp.start {
-  name = 'Godot',
+  name = "gdscript",
   cmd = cmd,
   root_dir = vim.fs.dirname(
     vim.fs.find({ 'project.godot', '.git' }, { upward = true })[1]
